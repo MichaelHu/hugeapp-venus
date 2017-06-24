95b783f40a5dfe2d2d66d4396f14d96baa3ba0af
hudamin <hdm258i@gmail.com>
2017-06-24 19:44:48 +0800

    add error-log

--- index.js
+++ index.js
@@ -3,12 +3,29 @@
 var KOA = require( 'koa' );
 var body = require( 'koa-better-body' );
 var router = require( 'koa-better-router' )().loadMethods();
+var cors = require( 'kcors' );
+var mongo = require( 'koa-mongo' );
 
 var app = new KOA();
+var port = 8222;
 
 router.get( '/list', function * ( next ) {
     this.body = 'list';
+    yield next;
 } );
+
+router.post( '/error-log', body(), function * ( next ) {
+    if ( this.request.fields ) {
+        yield this.mongo.db( 'venus' )
+            .collection( 'error-log' )
+            .insert( this.request.fields )
+            ; 
+    }
+
+    this.body = 'log success';
+    yield next;
+} );
+
 
 router.post( '/upload', body(), function * ( next ) {
 
@@ -16,16 +33,32 @@ router.post( '/upload', body(), function * ( next ) {
     console.log( this.request.fields );
     console.log( this.request.body );
 
+    if ( this.request.fields ) {
+        yield this.mongo.db( 'venus' )
+            .collection( 'error-log' )
+            .insert( this.request.fields )
+            ; 
+    }
+
     this.body = JSON.stringify( {
         fields: this.request.fields
         , files: this.request.files
         , body: this.request.body || null
-    }, null, 2 );
+    }, null, 4 );
 
     yield next;
 
 } );
 
+app.use( cors() );
+app.use( mongo( {
+    host: '127.0.0.1'
+    , port: 27017
+    , db: 'venus'
+    , user: 'hudamin'
+    , pass: 'test'
+} ) );
 app.use( router.middleware() );
-app.listen( 4292 );
-console.log( 'listening on 4292 ...' );
+app.listen( port, () => { 
+    console.log( 'listening on ' + port + ' ...' );
+} );
